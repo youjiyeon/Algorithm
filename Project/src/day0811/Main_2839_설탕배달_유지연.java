52dbce1f135e174a6b722004e69e61feb2d82c57
유지연 <jiyeonyou0416@naver.com>
2022-08-11 17:01:58 +0900

    0811 과제제출

---
+++ Project/src/day0811/Main_2839_설탕배달_유지연.java
@@ -0,0 +1,49 @@
+package day0811;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_2839_설탕배달_유지연 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int input = Integer.parseInt(br.readLine());
+		
+		
+		int temp;
+		int result;
+		int i = 1;
+		
+		while(true)
+		{
+			temp = input - (3*i);
+			
+			if(temp<0)
+			{
+				result = -1;
+				break;
+			}
+			
+			if((temp%5)==0)
+			{
+				result = i + (temp / 5);
+				break;
+			}
+			else 
+			{
+				if((input%5)==0)
+				{
+					result = input/5;
+					break;
+				}
+			}
+			
+			i++;
+		}
+		System.out.print(result);
+
+	}
+
+}
