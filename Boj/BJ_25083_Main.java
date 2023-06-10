6665b79ae67958d4ad5c28d687034a7228270e3d
유지연 <57094856+youjiyeon@users.noreply.github.com>
2023-06-10 16:17:33 +0900

    Add files via upload

---
+++ Boj/BJ_25083_Main.java
@@ -0,0 +1,10 @@
+public class BJ_25083_Main {
+    public static void main(String[] args) {
+        System.out.println("         ,r\'\"7");
+        System.out.println("r`-_   ,\'  ,/");
+        System.out.println(" \\. \". L_r\'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
