bd27a09d95f405c6131d4ffe7631caececd8b22a
유지연 <57094856+youjiyeon@users.noreply.github.com>
2023-06-10 16:13:56 +0900

    Add files via upload

---
+++ Boj/BJ_11382_Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class BJ_11382_Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+
+        System.out.println(a+b+c);
+    }
+}
