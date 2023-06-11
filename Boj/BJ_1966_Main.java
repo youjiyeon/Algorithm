79045fb3beaf97d57d94b951afbe4736435955d1
유지연 <57094856+youjiyeon@users.noreply.github.com>
2023-06-11 23:47:29 +0900

    Create BJ_1966_Main.java

---
+++ Boj/BJ_1966_Main.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class BJ_1966_Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int t = 1; t <= T; t++) 
+		{
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken()); // 문서의 갯수
+			int M = Integer.parseInt(st.nextToken()); // 궁금한 문서의 위치
+			Queue<Integer> queue = new LinkedList<Integer>();
+			PriorityQueue<Integer> priqueue = new PriorityQueue<Integer>(Collections.reverseOrder());
+			
+			//int target = 0; // 궁금한 문서 값 저장
+			int cnt = 0; // 몇번째로 인쇄되는 지
+			
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) 
+			{
+				int in = Integer.parseInt(st.nextToken());
+/*				if(i==M)
+					target = in;*/
+				priqueue.add(in);
+				queue.add(in);
+			}
+			
+			while(true)//!queue.isEmpty() && !priqueue.isEmpty())
+			{
+				
+				int out = queue.poll();
+				
+				// 인쇄하는 경우
+				if(out == priqueue.peek()) 
+				{
+					//System.out.println("Print "+M+" "+out+" "+priqueue.peek());
+					priqueue.poll();
+					cnt++;
+					if(M==0)
+						break;
+					M--;
+					if(M<0)
+						M=queue.size()-1;
+				}
+				// 인쇄 안하는 경우 -> 다시 queue로 add
+				else
+				{
+					//System.out.println("No print "+M+" "+out+" "+priqueue.peek());
+					queue.add(out);
+					M--;
+					if(M<0)
+						M=queue.size()-1;
+				}
+
+			}
+			
+			System.out.println(cnt);
+		}
+
+	}
+
+}
