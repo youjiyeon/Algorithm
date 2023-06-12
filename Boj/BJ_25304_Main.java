d29253a8d65b6389104dde0ad41992298b829ef0
유지연 <57094856+youjiyeon@users.noreply.github.com>
2023-06-12 23:41:09 +0900

    Update BJ_25304_Main.java

--- Boj/BJ_25304_Main.java
+++ Boj/BJ_25304_Main.java
@@ -14,15 +14,17 @@ public class BJ_25304_Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int val = Integer.parseInt(st.nextToken());
             int cnt = Integer.parseInt(st.nextToken());
-
+            // 원본 값에서 사용한 값*갯수만큼 빼주기
             total -= (val*cnt);
         }
         // 
         
         // 결과 출력
+        // 빼서 원본의 값이랑 일치하면 Yes
         if(total==0){
             System.out.println("Yes");
         }
+        // 아니면 No
         else System.out.println("No");
         //
     }
