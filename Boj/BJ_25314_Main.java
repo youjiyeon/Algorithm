0fe48dafdb28b2859c4297931989364f2893ca5f
유지연 <57094856+youjiyeon@users.noreply.github.com>
2023-06-13 19:07:27 +0900

    Update BJ_25314_Main.java

--- Boj/BJ_25314_Main.java
+++ Boj/BJ_25314_Main.java
@@ -5,8 +5,11 @@ public class BJ_25314_Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
+		// 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int val = Integer.parseInt(br.readLine());
+		//
+		
 		int n = val/4;
 		for(int i=0;i<n;i++){
 			System.out.print("long ");
