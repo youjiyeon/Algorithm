03a9e652cf3fca1a6f6ae85785ae1da04c08fb5e
유지연 <57094856+youjiyeon@users.noreply.github.com>
2023-06-14 23:48:00 +0900

    Add files via upload

---
+++ Boj/BJ_5597_Main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_5597_Main {
+    public static boolean[] visit;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        visit = new boolean[31];
+        for (int i = 0; i < 28; i++) {
+            int val = Integer.parseInt(br.readLine());
+            visit[val] = true;
+        }
+
+        for (int i = 1; i < 31; i++) {
+            if(!visit[i]){
+                System.out.println(i);
+            }
+        }
+    }
+
+}
