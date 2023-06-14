fce1b9681783b18960c4b11abea7233e815985dc
유지연 <57094856+youjiyeon@users.noreply.github.com>
2023-06-14 23:50:46 +0900

    Update BJ_5597_Main.java

--- Boj/BJ_5597_Main.java
+++ Boj/BJ_5597_Main.java
@@ -24,6 +24,7 @@ public class BJ_5597_Main {
         //
         
         // 결과 출력
+        // 오름차순으로 검사
         for (int i = 1; i < 31; i++) {
             if(!visit[i]){
                 System.out.println(i);
