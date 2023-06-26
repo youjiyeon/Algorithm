fbb579bdb98d6f6c294637555d6d8fa4f089787f
유지연 <57094856+youjiyeon@users.noreply.github.com>
2023-06-27 01:22:08 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18196 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,33 @@
+
+
+import java.io.*;
+
+public class Main {
+
+	static int[] DP = new int[1000001];
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		//Scanner s = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		for (int i = 2; i <= N; i++) 
+		{
+			DP[i] = DP[i-1]+1;
+			
+			// 3으로 나눠진다
+			if (i % 3 == 0) 
+			{
+				DP[i] = Math.min(DP[i], DP[i/3]+1);
+			} 
+			// 2로 나눠진다
+			if (i % 2 == 0) 
+			{
+				DP[i] = Math.min(DP[i], DP[i/2]+1);
+			}
+		}
+		
+		System.out.println(DP[N]);
+	}
+
+}
