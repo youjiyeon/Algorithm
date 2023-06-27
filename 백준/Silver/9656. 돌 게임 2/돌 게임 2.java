ac62ec2bdb2887c061440b07ff811d969dc98c62
유지연 <57094856+youjiyeon@users.noreply.github.com>
2023-06-27 16:36:25 +0900

    [Silver IV] Title: 돌 게임 2, Time: 128 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int val = Integer.parseInt(br.readLine());
+
+        int temp;
+        int result;
+        int i = 1;
+
+        while (true){
+            temp = val - (1*i);
+
+            if(temp%3==0){
+                result = i+(temp/3);
+                break;
+            }
+            else {
+                if(val%3==0){
+                    result = val/3;
+                    break;
+                }
+            }
+            i++;
+        }
+
+        if(result%2!=0){
+            System.out.println("CY");
+        }
+        else System.out.println("SK");
+    }
+}
