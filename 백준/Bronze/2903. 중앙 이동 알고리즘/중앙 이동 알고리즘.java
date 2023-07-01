9b6c3548bd5535b5fe9dbe71f28ba68058504a06
유지연 <57094856+youjiyeon@users.noreply.github.com>
2023-07-01 10:55:23 +0900

    Update 중앙 이동 알고리즘.java

--- 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
+++ 백준/Bronze/2903. 중앙 이동 알고리즘/중앙 이동 알고리즘.java
@@ -9,6 +9,7 @@ public class Main {
         
         // Math의 클래스의 pow()메서드를 사용했다.
         // Math.pow(거듭제곱할 숫자, 거듭제곱(몇제곱인지);
+        // 각 변의 점의 개수의 제곱
         System.out.println((int)Math.pow(Math.pow(2, n) +1, 2));
     }
 }
