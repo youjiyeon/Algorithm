d394b2ccd643e79174095d7a24a57a8fda36f685
유지연 <57094856+youjiyeon@users.noreply.github.com>
2023-07-20 15:40:26 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 692 ms, Memory: 128556 KB -BaekjoonHub

--- 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.java
@@ -6,62 +6,59 @@ import java.util.*;
 public class Main {
     public static int N;
     public static int[] edge, result;
-    public static List<Integer>[] lists;
+    public static List<Integer>[] list;
+    public static Queue<Integer> q = new LinkedList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         edge = new int[N+1];
         result = new int[N+1];
-        lists = new List[N+1];
-
-        for (int i = 0; i <= N; i++) {
-            lists[i] = new ArrayList<>();
+        list = new List[N+1];
+        for (int i = 1; i <= N; i++) {
+            list[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken());
             int to = Integer.parseInt(st.nextToken());
-            lists[from].add(to);
+            list[from].add(to);
             edge[to]++;
         }
 
-        Queue<Integer> q = new LinkedList<>();
+        // 진입 차수가 없을 시 큐에 넣기
         for (int i = 1; i <= N; i++) {
-            if (edge[i] == 0) {
+            if (edge[i]==0){
                 q.offer(i);
             }
         }
 
+        // 위상 정렬
+        sort();
+        for (int i = 1; i <= N; i++) {
+            System.out.print(result[i]+" ");
+        }
+    }
+    public static void sort(){
         int cnt = 1;
-        while (!q.isEmpty()) {
+        while (!q.isEmpty()){
             int size = q.size();
 
             for (int s = 0; s < size; s++) {
-                // 큐에서 노드번호 꺼내기
-                int nodeNo = q.poll();
-                result[nodeNo] = cnt;
+                int cur = q.poll();
+                result[cur] = cnt;
+                for (int i = 0; i < list[cur].size(); i++) {
+                    edge[list[cur].get(i)]--;
 
-                // 꺼낸 노드의 인접한 노드들 찾기
-                //List<Integer> list = lists[nodeNo];
-
-                // 인접한 노드의 개수만큼 반복문 실행
-                for (int i = 0; i < lists[nodeNo].size(); i++) {
-                    // 인접한 노드 진입차수 갱신
-                    edge[lists[nodeNo].get(i)] -- ;
-                    // 갱신된 노드의 진입차수가 0이면 큐에 노드 넣기
-                    if (edge[lists[nodeNo].get(i)] == 0) {
-                        q.offer(lists[nodeNo].get(i));
+                    if (edge[list[cur].get(i)] == 0) {
+                        q.offer(list[cur].get(i));
                     }
                 }
             }
             cnt++;
-        }
-
-        for (int i = 1; i <= N; i++) {
-            System.out.print(result[i]+" ");
         }
     }
 }
