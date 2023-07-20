78750d280cb4ab7cdf6ff2c933e8811165497d6b
유지연 <57094856+youjiyeon@users.noreply.github.com>
2023-07-20 16:16:18 +0900

    [Gold II] Title: 문제집, Time: 1124 ms, Memory: 49620 KB -BaekjoonHub

---
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int N;
+    public static int[] edge;
+    public static List<Integer> result = new ArrayList<>();
+    public static List<Integer>[] list;
+    public static PriorityQueue<Integer> q = new PriorityQueue<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        edge = new int[N+1];
+        list = new List[N+1];
+        for (int i = 1; i <= N; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            list[from].add(to);
+            edge[to]++;
+        }
+
+        // 진입 차수가 없을 시 큐에 넣기
+        for (int i = 1; i <= N; i++) {
+            if (edge[i]==0){
+                q.offer(i);
+            }
+        }
+
+        // 위상 정렬
+        sort();
+
+
+        for (int i = 0; i < result.size(); i++) {
+            System.out.print(result.get(i)+" ");
+        }
+    }
+    public static void sort(){
+        while (!q.isEmpty()){
+            int cur = q.poll();
+            result.add(cur);
+            for (int i = 0; i < list[cur].size(); i++) {
+                edge[list[cur].get(i)]--;
+
+                if (edge[list[cur].get(i)] == 0) {
+                    q.offer(list[cur].get(i));
+                }
+            }
+        }
+    }
+}
