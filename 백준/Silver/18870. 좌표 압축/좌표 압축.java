6b88feed85c28d9cb470221fe9844f96a838e69c
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2023-08-15 01:35:02 +0900

    [Silver II] Title: 좌표 압축, Time: 1892 ms, Memory: 202152 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static int N;
+    public static int[] num, temp;
+    public static StringBuilder sb = new StringBuilder();
+    public static List<Integer> list = new ArrayList<>();
+    public static Set<Integer> set = new LinkedHashSet<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        num = new int[N];
+        temp = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+            temp[i] = num[i];
+        }
+
+        Arrays.sort(temp);
+        for (int i = 0; i < N; i++) {
+            if (i==0 || temp[i-1]!=temp[i]){
+                list.add(temp[i]);
+            }
+        }
+        
+        for (int i = 0; i < N; i++) {
+            bs(num[i]);
+        }
+        System.out.println(sb);
+    }
+
+    public static void bs(int target){
+        int l = 0;
+        int r = list.size()-1;
+        int mid = 0;
+        while (l<=r){
+            mid = (l+r)/2;
+            if (list.get(mid)<target){
+                l = mid+1;
+            } else if (list.get(mid)>target) {
+                r = mid-1;
+            } else {
+                sb.append(mid+" ");
+                return;
+            }
+        }
+    }
+}
