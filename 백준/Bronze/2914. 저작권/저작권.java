74df5094cbfa33d5a367e2fb73cfb2da41f3f9ba
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2023-08-20 18:54:18 +0900

    [Bronze III] Title: 저작권, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int i = Integer.parseInt(st.nextToken());
+        int j = Integer.parseInt(st.nextToken());
+        System.out.println(i*j-i+1);
+    }
+}
