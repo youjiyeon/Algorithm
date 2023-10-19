3f3d443999f32f0faa8cdbb1abe23155ddeb7767
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2023-10-19 20:18:51 +0900

    [Silver V] Title: 장난감 경주, Time: 160 ms, Memory: 16136 KB -BaekjoonHub

--- 백준/Silver/19592. 장난감 경주/장난감 경주.java
+++ 백준/Silver/19592. 장난감 경주/장난감 경주.java
@@ -1,59 +1,59 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
-// 각각의 경주 시간을 구하고
-// 3번째의 경우 0~Y까지 이분탐색해서 min이 나올때까지
+import java.util.StringTokenizer;
 
 public class Main {
-    public static int V, N, X, Y, max;
+    public static int N, X, Y;
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < T; i++) {
+        for (int t = 0; t < T; t++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+            // 참가자 수
             N = Integer.parseInt(st.nextToken());
+            // 총 트랙 길이
             X = Integer.parseInt(st.nextToken());
+            // 부스터 제한
             Y = Integer.parseInt(st.nextToken());
-            max = Integer.MIN_VALUE;
 
+            // 속도 저장 배열
+            // N-1은 나
+            int max = Integer.MIN_VALUE;
+            int V;
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N-1; j++) {
+            for (int i = 0; i < N-1; i++) {
                 V = Integer.parseInt(st.nextToken());
                 max = Math.max(max, V);
             }
-            double com = 1.0*X/max;
+
+            double min = 1.0*X/max;
             V = Integer.parseInt(st.nextToken());
 
             if (V>max){
-                sb.append(0+"\n");
-            } else if (1+1.0*((X-Y)/V)>=com) {
-                sb.append(-1+"\n");
+                sb.append(0);
+            }
+            else if ((1+1.0*(X-Y)/V)>=min) {
+                sb.append(-1);
             }
             else {
-                int left = 0;
-                int right = Y;
-                int mid = 0;
-                while (true){
-                    if (left>right)
-                        break;
-
-                    mid = (left+right)/2;
-                    double mt = 1+1.0*(X-mid)/V;
-                    if(mt>=com){
-                        left = mid+1;
-                    }
-                    else {
-                        right = mid-1;
-                    }
+                // 부스터 값 범위
+                int l = 0;
+                int r = Y;
+                int m = 0;
+                while (l <= r) {
+                    m = (l + r) / 2;
+                    double temp = 1+1.0 * (X - m) / V;
+                    if (min <= temp) {
+                        l = m + 1;
+                    } else
+                        r = m - 1;
                 }
-                sb.append(left+"\n");
+                sb.append(l);
             }
+            sb.append("\n");
         }
         System.out.println(sb);
     }
-
 }
