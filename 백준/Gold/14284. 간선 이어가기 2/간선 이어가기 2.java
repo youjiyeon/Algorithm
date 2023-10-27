06e9d34f7a8e24133ff95124c3ceea1d01de941b
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2023-10-28 01:11:46 +0900

    [Gold V] Title: 간선 이어가기 2, Time: 484 ms, Memory: 48304 KB -BaekjoonHub

--- 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.java
+++ 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.java
@@ -1,66 +1,67 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main
-{
+public class Main {
+    public static int n, m, s, e;
+    public static int[] dist;
     public static class Node{
-        int n;
-        int d;
+        int idx;
+        int val;
 
-        public Node(int n, int d) {
-            this.n = n;
-            this.d = d;
+        public Node(int next, int val) {
+            this.idx = next;
+            this.val = val;
         }
     }
-    public static int n;
-    public static List<Node>[] graph;
-    public static int[] dist;
+    public static List<Node>[] list;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-        graph = new List[n+1];
+        list = new List[n+1];
         dist = new int[n+1];
-        for (int i = 0; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            list[i] = new ArrayList<>();
         }
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
 
-            graph[a].add(new Node(b,c));
-            graph[b].add(new Node(a,c));
+            list[from].add(new Node(to,v));
+            list[to].add(new Node(from,v));
         }
 
         st = new StringTokenizer(br.readLine());
-        int s = Integer.parseInt(st.nextToken());
-        int e = Integer.parseInt(st.nextToken());
-        System.out.println(dijstra(s, e));
+        s = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
 
+        System.out.println(dijkstra());
     }
 
-    private static int dijstra(int s, int e) {
-        PriorityQueue<Node> q = new PriorityQueue<>((o1,o2)->o1.d-o2.d);
+    private static int dijkstra() {
+        PriorityQueue<Node> q = new PriorityQueue<>((o1,o2)->o1.val-o2.val);
         q.offer(new Node(s,0));
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[s] = 0;
 
         while (!q.isEmpty()){
             Node cur = q.poll();
+            if (cur.val>dist[cur.idx])
+                continue;
 
-            for (Node n: graph[cur.n]) {
-                if (dist[n.n]>dist[cur.n]+n.d){
-                    q.offer(n);
-                    dist[n.n]=dist[cur.n]+n.d;
+            for (Node n: list[cur.idx]) {
+                if(dist[n.idx]>n.val+dist[cur.idx]){
+                    dist[n.idx]=n.val+dist[cur.idx];
+                    q.offer(new Node(n.idx, dist[n.idx]));
                 }
             }
         }
-
         return dist[e];
     }
 }
