8c952d0658d5dd16902a78dbb3b01ffbca3a3c57
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2023-11-09 14:13:16 +0900

    [Bronze II] Title: 저항, Time: 132 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Bronze/1076. 저항/저항.java
+++ 백준/Bronze/1076. 저항/저항.java
@@ -1,99 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//첫번째 값과 두번째 값을 저장할 문자열 변수
-        String S = "";
-        //최종 값을 저장할 정수형 변수 
-		long N = 0;
+		//색을 순서대로 집어넣은 String 형의 배열
+		String[] color = {"black", "brown", "red", "orange", "yellow", "green",
+				"blue", "violet", "grey", "white"};
 		
-		for(int i = 0; i < 3; i++) {
-			String A = br.readLine();
-			
-            //입력값을 판단 후 해당 값을 연산.
-            //첫번째, 두번째 값과 세번째 값의 연산 방식이 다르기 때문에 조건문으로 분리
-			switch(A) {
-			case "black":
-				if(i == 2) {
-					N *= 1;
-				}else {
-					S += "0";
-				}
-			break;
-			case "brown":
-				if(i == 2) {
-					N *= 10;
-				}else {
-					S += "1";
-				}
-			break;
-			case "red":
-				if(i == 2) {
-					N *= 100;
-				}else {
-					S += "2";
-				}
-			break;
-			case "orange":
-				if(i == 2) {
-					N *= 1000;
-				}else {
-					S += "3";
-				}
-			break;
-			case "yellow":
-				if(i == 2) {
-					N *= 10000;
-				}else {
-					S += "4";
-				}
-			break;
-			case "green":
-				if(i == 2) {
-					N *= 100000;
-				}else {
-					S += "5";
-				}
-			break;
-			case "blue":
-				if(i == 2) {
-					N *= 1000000;
-				}else {
-					S += "6";
-				}
-			break;
-			case "violet":
-				if(i == 2) {
-					N *= 10000000;
-				}else {
-					S += "7";
-				}
-			break;
-			case "grey":
-				if(i == 2) {
-					N *= 100000000;
-				}else {
-					S += "8";
-				}
-			break;
-			case "white":
-				if(i == 2) {
-					N *= 1000000000;
-				}else {
-					S += "9";
-				}
-			break;
-			}
-            //두번째 값까지 연산이 되었다면 정수형에 형변환해서 저장
-			if(i == 1) {
-				N = Long.parseLong(S);
-			}
-		}
-		System.out.println(N);
+		String A = br.readLine();
+		String B = br.readLine();
+		String C = br.readLine();
+		
+		//인덱스값은 정수이기 때문에 처음 수에는 *10을 해주고 두번째 값과 더해준다.
+		long result = (Arrays.asList(color).indexOf(A) * 10) + Arrays.asList(color).indexOf(B);
+		//한번에 연산하기에 너무 길어지니 두줄로 나눈다.
+		//첫번째값과 두번째값에 해당한 값을 구했으니 그 값에 세번째 값을 곱해준다.
+		result *= Math.pow(10, Arrays.asList(color).indexOf(C));
+		
+		System.out.println(result);
 	}
 
 }
