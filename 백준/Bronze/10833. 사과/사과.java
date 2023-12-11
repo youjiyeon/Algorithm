e4d5022d43aab38e8f85b930768db4410a634c96
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2023-12-11 18:38:14 +0900

    [Bronze III] Title: 사과, Time: 128 ms, Memory: 14216 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		//남는 사과의 숫자를 저장할 변수
+		int sum = 0;
+		
+		for(int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			sum += (B % A);
+		}
+		System.out.println(sum);
+	}
+
+}
