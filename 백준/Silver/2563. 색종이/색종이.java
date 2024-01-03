af2f43e269849805ea8d4272a265052b2c37c925
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-01-04 01:47:34 +0900

    [Silver V] Title: 색종이, Time: 120 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,36 +1,27 @@
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] map = new int[101][101];
+        int cnt = 0;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int paperNum = Integer.parseInt(br.readLine());
-		int[][] papers = new int[100][100];
-		int cnt = 0;
-		
-		for (int i = 0; i < paperNum; i++) 
-		{
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int firstX = Integer.parseInt(st.nextToken());
-			int firstY = Integer.parseInt(st.nextToken());
-			
-			for (int x = firstX; x < firstX+10; x++) 
-			{
-				for (int y = firstY; y < firstY+10; y++) 
-				{
-					if(papers[x][y] != 1)
-					{
-						papers[x][y] = 1;
-						cnt++;
-					}
-				}	
-			}
-		}
-		System.out.println(cnt);
-		
-	}
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
+            for (int j = x; j < x+10; j++) {
+                for (int k = y; k < y+10; k++) {
+                    if (map[j][k]==0){
+                        cnt++;
+                        map[j][k] = 1;
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
 }
