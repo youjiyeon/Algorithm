c798e45ef6c2a944f232096c9f99eec95ad17c4e
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-01-03 22:58:42 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,36 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int paperNum = Integer.parseInt(br.readLine());
+		int[][] papers = new int[100][100];
+		int cnt = 0;
+		
+		for (int i = 0; i < paperNum; i++) 
+		{
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int firstX = Integer.parseInt(st.nextToken());
+			int firstY = Integer.parseInt(st.nextToken());
+			
+			for (int x = firstX; x < firstX+10; x++) 
+			{
+				for (int y = firstY; y < firstY+10; y++) 
+				{
+					if(papers[x][y] != 1)
+					{
+						papers[x][y] = 1;
+						cnt++;
+					}
+				}	
+			}
+		}
+		System.out.println(cnt);
+		
+	}
+
+}
