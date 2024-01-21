6c8d754b85c48e348ce815f536ecb70018025b41
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-01-21 18:19:00 +0900

    [Gold V] Title: 다이어트, Time: 164 ms, Memory: 16208 KB -BaekjoonHub

--- 백준/Gold/1484. 다이어트/다이어트.java
+++ 백준/Gold/1484. 다이어트/다이어트.java
@@ -1,34 +1,30 @@
-/*
-    while문 조건은 s<=n
-    s와 e를 처음엔 1로 하고
-    짝수가 아닌게 K를 넘어가면 or 끝에 도달하면
-    s++, e=s 초기화
- */
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int G;
-    public static List<Integer> list = new ArrayList<>();
     public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        G = Integer.parseInt(br.readLine());
+        int G = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
 
         int s = 1;
         int e = 2;
+        int tmp = 0;
         while (s<e){
+            tmp = (int)(Math.pow(e,2)-Math.pow(s,2));
             // G랑 일치하다면
-            if (Math.pow(e,2)-Math.pow(s,2)==G){
+            if (tmp==G){
                 list.add(e);
                 s++;
                 e++;
             }
             // G보다 작다면
-            else if (Math.pow(e,2)-Math.pow(s,2)<G){
+            else if (tmp<G){
                 e++;
             }
             // G보다 크다면
-            else if (Math.pow(e,2)-Math.pow(s,2)>G){
+            else if (tmp>G){
                 s++;
             }
         }
@@ -38,7 +34,8 @@ public class Main {
             System.exit(0);
         }
         for (int i:list) {
-            System.out.println(i);
+            sb.append(i).append("\n");
         }
+        System.out.println(sb);
     }
 }
