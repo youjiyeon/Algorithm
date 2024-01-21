36eccc85af4e4035223d0f3786b0e111e888c910
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-01-21 22:34:03 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,26 +1,40 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[9];
         int sum = 0;
-        for (int i = 0; i < 9; i++) {
+        int[] arr = new int[9];
+        for(int i=0; i<9; ++i) {
             arr[i] = Integer.parseInt(br.readLine());
             sum += arr[i];
         }
-        for (int i = 0; i < 8; i++) {
-            for (int j = i+1; j < 9; j++) {
-                if (sum - arr[i] - arr[j] == 100) {
-                    arr[i] = 0;
-                    arr[j] = 0;
-                    Arrays.sort(arr);
-                    for (int k = 2; k < 9; k++) {
-                        System.out.println(arr[k]);
-                    }
-                    return;
-                }
+
+        Arrays.sort(arr);
+        int findNum = sum - 100;
+        int left = 0;
+        int right = 8;
+        while(true) {
+            if(arr[left] + arr[right] == findNum) {
+                break;
+            }
+            if(findNum > arr[left] + arr[right]) {
+                left++;
+            }
+            else {
+                right--;
             }
         }
+
+        for(int i = 0; i < 9; i++) {
+            if(i==left || i==right) {
+                continue;
+            }
+            sb.append(arr[i]).append("\n");
+        }
+
+        System.out.println(sb);
     }
 }
