5eb019710bab62b3f6f8e9f8e4a07f34bc3b54c9
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-01-24 14:10:54 +0900

    [Gold IV] Title: 고냥이, Time: 164 ms, Memory: 15496 KB -BaekjoonHub

---
+++ 백준/Gold/16472. 고냥이/고냥이.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int cnt, result;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        cnt = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+
+        result = Integer.MIN_VALUE;
+        int[] check = new int[26]; // 알파벳 유무 확인
+        int left = 0;
+        int right = 0;
+        int length = 0;
+
+        check[str.charAt(0)-'a']++;
+        length++;
+        while (true){
+            right++;
+            if(right==str.length()) break;
+
+            check[str.charAt(right)-'a']++;
+
+            if(check[str.charAt(right)-'a'] == 1){
+                length++;
+            }
+
+            while (length>cnt){
+                check[str.charAt(left)-'a']--;
+
+                if(check[str.charAt(left)-'a'] == 0){
+                    length--;
+                }
+                left++;
+            }
+
+            result = Math.max(result, right-left+1);
+        }
+        System.out.println(result);
+    }
+}
