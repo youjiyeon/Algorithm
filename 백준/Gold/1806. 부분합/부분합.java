4535aab323d3d503c94f0f8535dd8ff879bca909
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-01-24 14:12:14 +0900

    [Gold IV] Title: 부분합, Time: 964 ms, Memory: 23804 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int N, S, min;
+    public static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        min = Integer.MAX_VALUE;
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            int cnt = 1;
+            int sum = arr[i];
+            int j = i+1;
+            while (true){
+                if(j>=N || sum>=S || cnt>=min){
+                    break;
+                }
+                sum += arr[j];
+                cnt++;
+                j++;
+            }
+            if(sum>=S) {
+                min = Math.min(min, cnt);
+            }
+        }
+        
+        if(min==Integer.MAX_VALUE) min = 0;
+        System.out.println(min);
+    }
+}
