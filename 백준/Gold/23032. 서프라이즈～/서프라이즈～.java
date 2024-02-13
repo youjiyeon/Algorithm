f5cf8afa0a439dc7e6d122afb181684e905ab6fa
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-02-13 11:35:03 -0500

    [Gold III] Title: 서프라이즈~, Time: 288 ms, Memory: 18388 KB -BaekjoonHub

--- 백준/Gold/23032. 서프라이즈～/서프라이즈～.java
+++ 백준/Gold/23032. 서프라이즈～/서프라이즈～.java
@@ -1,8 +1,9 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
@@ -14,30 +15,29 @@ public class Main {
 
         int min = Integer.MAX_VALUE;
         int sum = Integer.MIN_VALUE;
+        // 앞그룹
         for (int i = 1; i <= n-1; i++) {
+            // 뒷그룹
             for (int j = i+1; j <= n; j++) {
-                // 앞 그룹의 합
 
-                int s = i; //????
-                int e = j; //????
+                int s = i;
+                int e = j;
+                // 앞, 뒤 그룹의 구분자
                 int mid = 0;
                 while (s<=e){
                     mid = (s+e)/2;
-                    int tmp1 = arr[mid]-arr[i-1];
-                    int tmp2 = arr[j]-arr[mid];
-                    if (min>Math.abs(tmp1-tmp2)){
-                        min = Math.abs(tmp1-tmp2);
-                        sum = tmp1+tmp2;
+                    int front = arr[mid]-arr[i-1];
+                    int back = arr[j]-arr[mid];
+                    if (min>Math.abs(front-back)){
+                        min = Math.abs(front-back);
+                        sum = front+back;
                     }
-                    else if (min==Math.abs(tmp1-tmp2)) {
-                        sum = Math.max(sum,tmp1+tmp2);
+                    else if (min==Math.abs(front-back)) {
+                        sum = Math.max(sum, front+back);
                     }
-                    if (tmp1>tmp2) {
-                        e=mid-1;
-                    }
-                    else {
-                        s=mid+1;
-                    }
+                    // 앞 그룹이 더 크다면
+                    if (front>back) e = mid-1;
+                    else s = mid+1;
                 }
             }
         }
