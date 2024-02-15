2f217dfb113f8624dcb6496ac523e681739d4133
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-02-15 14:33:50 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Silver III/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int n, m;
+    public static int[] arr, pick;
+    public static boolean[] v;
+    public static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        pick = new int[m];
+        v = new boolean[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        perm(0);
+        System.out.println(sb);
+    }
+
+    public static void perm(int cnt){
+        if (cnt==m){
+            for (int i:pick) {
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (cnt!=0){
+                if (pick[cnt-1]>=arr[i]) continue;
+            }
+            if (!v[i]) {
+                v[i] = true;
+                pick[cnt] = arr[i];
+                perm(cnt + 1);
+                v[i] = false;
+            }
+        }
+    }
+}
