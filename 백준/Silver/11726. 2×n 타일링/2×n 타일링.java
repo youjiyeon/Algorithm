1871e919ef9cb52e2e1050ffd4515e4a69bbb3ba
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-03-17 01:27:52 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,20 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
+    public static final int MOD = 10007;
     public static void main(String[] args) throws IOException {
-        long[] dp = new long[1001];
-        // 초기값 설정
-        dp[1] = 1;
-        dp[2] = 2;
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
+        long[] dp = new long[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+
         for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i-1]+dp[i-2])%10007;
+            dp[i] = (dp[i-1] + dp[i-2]) % MOD;
         }
+
         System.out.println(dp[n]);
     }
 }
