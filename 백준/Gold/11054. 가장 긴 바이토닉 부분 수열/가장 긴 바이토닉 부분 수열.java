1302ef94696b337718583c13d903f72200e5d71b
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-03-20 15:59:10 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 152 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,43 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     public static int N;
-    public static int[] num, lis, lds;
+    public static int[] arr, lis, lds;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        num = new int[N];
+        arr = new int[N];
         lis = new int[N];
         lds = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            num[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
             lis[i] = lds[i] = 1;
         }
 
+        // 최장 증가 수열
         for (int i = 1; i < N; i++) {
             for (int j = 0; j < i; j++) {
-                if (num[i]>num[j])
+                if (arr[i] > arr[j])
                     lis[i] = Math.max(lis[i], lis[j]+1);
             }
         }
 
+        // 최장 감소 수열
         for (int i = N-1; i >= 0; i--) {
             for (int j = N-1; j >= i; j--) {
-                if (num[i]>num[j])
+                if (arr[i] > arr[j])
                     lds[i] = Math.max(lds[i], lds[j]+1);
             }
         }
 
-        int max = Integer.MIN_VALUE;
+        int ans = 0;
         for (int i = 0; i < N; i++) {
-            max = Math.max(max, lis[i]+lds[i]);
+            ans = Math.max(ans, lis[i]+lds[i]);
         }
 
-        System.out.println(max-1);
+        System.out.println(ans-1);
     }
 }
