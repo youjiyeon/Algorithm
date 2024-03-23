65dc5df3bb940c9240813e28378597ac62474b8b
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-03-24 02:34:55 +0900

    [Bronze III] Title: 더하기 3, Time: 136 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Bronze/11023. 더하기 3/더하기 3.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		//토큰의 개수를 저장한다.
+		int count = st.countTokens();
+		//값들의 합을 저장 할 변수
+		int sum = 0;
+		
+		for(int i = 0; i < count; i++) {
+			sum += Integer.parseInt(st.nextToken());
+		}
+		
+		System.out.println(sum);
+	}
+}
