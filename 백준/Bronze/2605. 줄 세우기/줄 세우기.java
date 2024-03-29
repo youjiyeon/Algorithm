e1847daf5414a112b79f8e4714ca8e60ec769628
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-03-30 02:00:52 +0900

    [Bronze II] Title: 줄 세우기, Time: 252 ms, Memory: 18492 KB -BaekjoonHub

---
+++ 백준/Bronze/2605. 줄 세우기/줄 세우기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Integer> list = new ArrayList<>();
+		int N = sc.nextInt();
+		
+		for(int i=0;i<N;i++) {
+			int num = sc.nextInt();
+			
+			if(num == 0) list.add(i+1);
+			else list.add(list.size()-num, i+1);
+		}
+		
+		for(int i :list)
+			System.out.print(i+" ");
+	}
+}
