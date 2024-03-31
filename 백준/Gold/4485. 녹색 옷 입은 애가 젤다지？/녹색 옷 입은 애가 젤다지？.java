18bfc7d81965477de5920dbe251a528fb9029eec
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-04-01 01:54:29 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 304 ms, Memory: 31944 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,69 +1,71 @@
 import java.io.*;
 import java.util.*;
 
-public class Main
-{
-    public static class Node{
-        int n;
-        int d;
-
-        public Node(int n, int d) {
-            this.n = n;
-            this.d = d;
-        }
-    }
-    public static int n, m;
-    public static int[][] map, dist,
-    way = {{0,1},{0,-1},{1,0},{-1,0}};
+public class Main {
+    public static int N;
+    public static int[][] map, dp,
+            way = {{0, 1},{0, -1},{1, 0},{-1, 0}};
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = 1;
         while (true) {
-            n = Integer.parseInt(br.readLine());
-            if(n==0) break;
+           N = Integer.parseInt(br.readLine());
+           if (N==0) break;
 
-            map = new int[n][n];
-            dist = new int[n][n];
-            for (int i = 0; i < n; i++) {
+           map = new int[N][N];
+           dp = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                Arrays.fill(dp[i], Integer.MAX_VALUE);
+            }
+
+            for (int i = 0; i < N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < n; j++) {
+                for (int j = 0; j < N; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-            sb.append("Problem "+t+": ").append(dijkstra()).append("\n");
+
+            bfs();
+
+/*            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    System.out.print(dp[i][j]+" ");
+                }
+                System.out.println();
+            }*/
+
+            sb.append("Problem ").append(t).append(": ").append(dp[N-1][N-1]).append("\n");
             t++;
         }
-        System.out.println(sb);
+
+        System.out.print(sb);
     }
 
-    private static int dijkstra() {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
-        pq.offer(new int[] {0,0,map[0][0]});
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-        dist[0][0] = map[0][0];
+    public static void bfs() {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[] {0, 0});
 
-        while (!pq.isEmpty()){
-            int x = pq.peek()[0];
-            int y = pq.peek()[1];
-            pq.poll();
+        dp[0][0] = map[0][0];
+
+        while (!q.isEmpty()) {
+            int x = q.peek()[0];
+            int y = q.peek()[1];
+            q.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nx = x + way[i][0];
-                int ny = y + way[i][1];
+                int nx = x+way[i][0];
+                int ny = y+way[i][1];
 
-                if(nx<0 || nx>=n || ny<0 || ny>= n){
+                if (nx<0 || nx>=N || ny<0 || ny>=N) {
                     continue;
                 }
 
-                if(dist[nx][ny]>dist[x][y]+map[nx][ny]){
-                    dist[nx][ny]=dist[x][y]+map[nx][ny];
-                    pq.offer(new int[] {nx, ny, map[nx][ny]});
+                if (dp[nx][ny] > dp[x][y]+map[nx][ny]) {
+                    dp[nx][ny] = dp[x][y]+map[nx][ny];
+                    q.offer(new int[] {nx, ny});
                 }
             }
         }
-        return dist[n-1][n-1];
     }
 }
