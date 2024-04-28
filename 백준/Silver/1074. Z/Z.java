13b7835b153b1a7f3dc33d819c0471ce9553c270
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-04-29 01:01:18 +0900

    [Silver I] Title: Z, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int ans = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        
+        int size = (int) Math.pow(2, N);
+        find(size, r, c);
+
+        System.out.println(ans);
+    }
+
+    public static void find(int size, int r, int c) {
+        if (size == 1) return;
+
+        if (r<size/2 && c<size/2) {
+            find(size/2, r, c);
+        }
+        else if (r<size/2 && c>=size/2) {
+            ans += size*size/4;
+            find(size/2, r, c-size/2);
+        }
+        else if (r>=size/2 && c<size/2) {
+            ans += (size*size/4) * 2;
+            find(size/2, r-size/2, c);
+        }
+        else {
+            ans += (size*size/4) * 3;
+            find(size/2, r-size/2, c-size/2);
+        }
+    }
+}
