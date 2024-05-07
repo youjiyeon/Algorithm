02a81c2f1530715808684f2546ea2072f970fc83
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-05-07 18:27:39 +0900

    [Gold V] Title: 알약, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Gold/4811. 알약/알약.java
+++ 백준/Gold/4811. 알약/알약.java
@@ -2,19 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int cnt, n;
+    public static int n;
     public static long[][] dp;
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         dp = new long[31][62];
         n = Integer.parseInt(br.readLine());
-
+        
         while (n != 0) {
-            cnt = 0;
-            recur(n, 0);
             sb.append(recur(n, 0)).append("\n");
-
             n = Integer.parseInt(br.readLine());
         }
 
