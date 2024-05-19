d3f7f27c40bea1c08cf3088b69e898da170b3637
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-05-19 15:16:53 +0900

    [Gold III] Title: 소용돌이 예쁘게 출력하기, Time: 188 ms, Memory: 15172 KB -BaekjoonHub

--- 백준/Gold/1022. 소용돌이 예쁘게 출력하기/소용돌이 예쁘게 출력하기.java
+++ 백준/Gold/1022. 소용돌이 예쁘게 출력하기/소용돌이 예쁘게 출력하기.java
@@ -1,53 +1,50 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Main {
+public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-        int r1 = Integer.parseInt(tokenizer.nextToken());
-        int c1 = Integer.parseInt(tokenizer.nextToken());
-        int r2 = Integer.parseInt(tokenizer.nextToken());
-        int c2 = Integer.parseInt(tokenizer.nextToken());
-        reader.close();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int r1 = Integer.parseInt(st.nextToken());
+        int c1 = Integer.parseInt(st.nextToken());
+        int r2 = Integer.parseInt(st.nextToken());
+        int c2 = Integer.parseInt(st.nextToken());
 
-        int[][] vortex = new int[r2-r1+1][c2-c1+1];
+        int[][] arr = new int[r2-r1+1][c2-c1+1];
         int max = 0;
-        for(int i=r1; i<=r2; i++){
-            for(int j=c1; j<=c2; j++){
-                vortex[i-r1][j-c1] = calculate(i, j);
-                max = Math.max(max, vortex[i-r1][j-c1]);
+        for (int i = r1; i <= r2; i++) {
+            for (int j = c1; j <= c2; j++) {
+                arr[i-r1][j-c1] = play(i, j);
+                max = Math.max(max, arr[i-r1][j-c1]);
             }
         }
 
-        StringBuilder formatter = new StringBuilder("%");
-        formatter.append(String.valueOf(max).length());
-        formatter.append("d ");
+        StringBuilder sb = new StringBuilder("%");
+        sb.append(String.valueOf(max).length());
+        sb.append("d ");
 
-        for(int i=0; i<=r2-r1; i++){
-            for(int j=0; j<=c2-c1; j++){
-                System.out.printf(formatter.toString(), vortex[i][j]);
+        for (int i = 0; i <= r2-r1; i++) {
+            for (int j = 0; j <= c2-c1; j++) {
+                System.out.printf(sb.toString(), arr[i][j]);
             }
             System.out.println();
         }
     }
 
-    static int calculate(int row, int column){
-        int border = Math.max(Math.abs(row),Math.abs(column));
-        int min = (int) Math.pow(2*border-1,2)+1;
+    static int play(int r, int c) {
+        int border = Math.max(Math.abs(r), Math.abs(c));
+        int min = (int)Math.pow(2*border-1, 2)+1;
 
-        if(row == border){
-            return min + 7*border -1 + column;
+        if (r == border) {
+            return min + 7*border -1 + c;
+        }
+        if (c == -border) {
+            return min + 5*border -1 + r;
+        }
+        if (r == -border) {
+            return min + 3*border -1 - c;
         }
 
-        if(column == -border){
-            return min + 5*border -1 + row;
-        }
-
-        if(row == -border){
-            return min + 3*border -1 - column;
-        }
-
-        return min + border -1 - row;
+        return min + border -1 - r;
     }
 }
