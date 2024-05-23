8ecc86513f84667acbccb518ef2197f57c5ec335
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-05-24 01:57:33 +0900

    [Bronze III] Title: 주사위, Time: 292 ms, Memory: 19600 KB -BaekjoonHub

---
+++ 백준/Bronze/9295. 주사위/주사위.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(String.format("Case %d: %d\n", i, a+b));
+        }
+        System.out.println(sb);
+    }
+}
