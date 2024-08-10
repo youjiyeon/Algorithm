29cc1cb37a9b86b1bbd638436fb3809f0347a82c
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-08-10 19:41:39 +0900

    [Gold I] Title: K번째 수, Time: 132 ms, Memory: 14416 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.java
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -12,16 +12,15 @@ class Main {
         long mid = (l+r)/2;
         long cnt = 0;
 
-        while (l<r){
+        while (l<r) {
             mid = (l+r)/2;
             cnt = 0;
 
             for (int i = 1; i <= n; i++) {
-                // n을 초과하면 안되서
                 cnt+=Math.min(mid/i, n);
             }
-            // cnt가 많다는 것은 임의의 x(mid)보다 작은 수가 b[k]보다 많다는 뜻
-            if (cnt>=k){
+            
+            if (cnt >= k) {
                 r = mid;
             }
             else l = mid+1;
