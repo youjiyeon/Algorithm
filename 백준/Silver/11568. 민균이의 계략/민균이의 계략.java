a3a14ac5040d1711f782271eb4ff76cb7365fe32
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-11-11 22:36:50 +0900

    [Silver II] Title: 민균이의 계략, Time: 124 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Silver/11568. 민균이의 계략/민균이의 계략.java
+++ 백준/Silver/11568. 민균이의 계략/민균이의 계략.java
@@ -2,32 +2,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    public static void main(String[] args) throws IOException {
+    static int n;
+    static int[] arr, lis;
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        int[] dp = new int[n];
-        int ans = 0;
+        n = Integer.parseInt(br.readLine());
 
+        arr = new int[n];
+        lis = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            dp[i] = 1;
         }
 
+        lis[0] = arr[0];
+
+        int len = 1;
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if(arr[j] < arr[i]){
-                    dp[i] = Math.max(dp[j]+1, dp[i]);
-                }
+            if (lis[len-1] < arr[i]) {
+                lis[len++] = arr[i];
+            }
+            else if (lis[len-1] > arr[i]) {
+                int idx = binary(0, len-1, arr[i]);
+                lis[idx] = arr[i];
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            ans = Math.max(ans, dp[i]);
+        System.out.println(len);
+    }
+
+    static int binary(int left, int right, int target) {
+        int mid;
+
+        while (left < right) {
+            mid = (left+right)/2;
+
+            if (lis[mid] < target) {
+                left = mid+1;
+            }
+            else {
+                right = mid;
+            }
         }
 
-        System.out.println(ans);
+        return right;
     }
 }
