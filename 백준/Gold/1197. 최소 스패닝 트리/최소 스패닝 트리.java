ce00db91a9aa538293b9568b81bc17b170206bd0
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-11-18 17:51:01 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 536 ms, Memory: 50572 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,80 +1,79 @@
-
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int V, E;
+    static int[] parents;
+    static class Node implements Comparable<Node> {
+        int from; int to; int cost;
 
-	static int V, E;
-	static int cnt;
-	static long result = 0;
-	static int[] num;
-	static int[][] arr;
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		
-		num = new int[V+1];
-		arr = new int[E][3]; //0 from, 1 to, 2 weight
-		
-		for (int i = 0; i < E; i++) 
-		{
-			st = new StringTokenizer(br.readLine());
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
-			
-			arr[i][0] = from;
-			arr[i][1] = to;
-			arr[i][2] = weight;
-		}
-		
-		for (int i = 1; i <= V; i++) 
-		{
-			num[i] = i;
-		}
-		Arrays.sort(arr, (a,b) -> {
-			return a[2] - b[2];
-		});
-/*		
-		for (int i = 1; i <= V; i++) {
-			System.out.println(Arrays.toString(arr[i]));
-		}*/
+        public Node(int from, int to, int cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
 
-		for (int i = 0; i < E; i++) 
-		{
-			int[] current = arr[i];
-			if(FindSet(current[0]) == FindSet(current[1])) continue;
-			
-			
-			Union(current[0],current[1]);
-			result += current[2];
-			cnt++;
-			if(cnt == V-1)
-				break;
-			
+        @Override
+        public int compareTo(Node o) {
+            return this.cost - o.cost;
+        }
+    }
+    static List<Node> list = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		}
-		System.out.println(result);
-	}
-	private static void Union(int i, int j) {
-		
-		if(FindSet(i) == FindSet(j))
-			return;
-		else
-			num[FindSet(i)] = FindSet(j);
-		
-	}
-	private static int FindSet(int i) {
-		
-		if(num[i] == i)
-			return i;
-		else
-			return num[i] = FindSet(num[i]);
-	}
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        parents = new int[V+1];
+
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+
+            list.add(new Node(from, to, cost));
+        }
+
+        set();
+        Collections.sort(list);
+
+        int sum = 0;
+        int cnt = 0;
+        for (Node n : list) {
+            if (union(n.from, n.to)) {
+                sum += n.cost;
+                cnt++;
+
+                if (cnt == E-1) break;
+            }
+        }
+
+        System.out.println(sum);
+    }
+
+    static void set() {
+        for (int i = 1; i <= V; i++) {
+            parents[i] = i;
+        }
+    }
+    static boolean union(int from, int to) {
+        int fromRoot = find(from);
+        int toRoot = find(to);
+
+        if (fromRoot==toRoot) return false;
+        else {
+            parents[toRoot] = fromRoot;
+            return true;
+        }
+    }
+
+    static int find(int v) {
+        if (parents[v] == v) return v;
+        else return parents[v] = find(parents[v]);
+    }
 
 }
