7b1d67543d085f6cb09e7d526f7045b9b7d412ff
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2024-11-19 10:55:31 +0900

    [Gold IV] Title: 해킹, Time: 820 ms, Memory: 155748 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.java
+++ 백준/Gold/10282. 해킹/해킹.java
@@ -1,17 +1,14 @@
-import org.w3c.dom.Node;
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, cnt;
     static class Node implements Comparable<Node>{
         int idx, time;
         public Node(int idx, int time) {
             this.idx = idx;
             this.time = time;
         }
-
+        
         @Override
         public int compareTo(Node o) {
             return this.time - o.time;
@@ -19,19 +16,15 @@ public class Main {
     }
     static List<Node>[] list;
     static int[] dp;
-    static boolean[] v;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
 
         while (t-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
             int d = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken()); // start
-
-            cnt = 0;
-            v = new boolean[n+1];
 
             dp = new int[n+1];
             Arrays.fill(dp, Integer.MAX_VALUE);
@@ -47,11 +40,11 @@ public class Main {
                 int from = Integer.parseInt(st.nextToken());
                 int time = Integer.parseInt(st.nextToken());
 
-                //list[to].add(new Node(from, time));
                 list[from].add(new Node(to, time));
             }
 
             dijkstra(c);
+            int cnt = 0;
             int max = 0;
             for (int i = 1; i <= n; i++) {
                 if (dp[i]==Integer.MAX_VALUE) continue;
@@ -66,24 +59,17 @@ public class Main {
         dp[start] = 0;
         PriorityQueue<Node> pq = new PriorityQueue<>();
         pq.offer(new Node(start, 0));
-        //v[start] = true;
 
         while (!pq.isEmpty()) {
             Node cur = pq.poll();
-            //cnt++;
 
-            if (v[cur.idx]) continue;
+            if (dp[cur.idx] < cur.time) continue;
 
-            v[cur.idx] = true;
             for (Node next : list[cur.idx]) {
-                if (v[next.idx]) continue;
-
                 int nextDist = next.time+dp[cur.idx];
                 if (dp[next.idx] > nextDist) {
                     dp[next.idx] = nextDist;
                     pq.offer(new Node(next.idx, nextDist));
-                    //v[next.idx] = true;
-                    //cnt++;
                 }
             }
         }
