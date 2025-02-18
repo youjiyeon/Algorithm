71787faef31f03eb9b5c9cc5d745f0626b7541cb
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2025-02-18 15:14:06 +0900

    [Gold III] Title: ACM Craft, Time: 988 ms, Memory: 189564 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -1,121 +1,61 @@
-/*
-* 문제 설명?
-* 각 edge도 저장하고
-* list도 저장(인접)
-* 시간도 저장
-*
- */
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
-import java.util.concurrent.LinkedBlockingQueue;
 
-public class Main {
-    public static class Node{
-        int edge;
-        int time;
-
-        int max;
-        public Node(int edge, int time, int max) {
-            this.edge = edge;
-            this.time = time;
-            this.max = max;
-        }
-
-        public int getEdge() {
-            return edge;
-        }
-
-        public void setEdge(int edge) {
-            this.edge = edge;
-        }
-
-        public int getTime() {
-            return time;
-        }
-
-        public void setTime(int time) {
-            this.time = time;
-        }
-
-        public int getMax() {
-            return max;
-        }
-
-        public void setMax(int max) {
-            this.max = max;
-        }
-
-        @Override
-        public String toString() {
-            return "Node{" +
-                    "edge=" + edge +
-                    ", time=" + time +
-                    '}';
-        }
-    }
-    public static int N;
-    public static Node[] nodes;
-    public static List<Integer>[] lists;
-    public static void main(String[] args) throws IOException {
+class Main {
+    public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
-        for (int t = 0; t < T; t++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            int K = Integer.parseInt(st.nextToken());
-            nodes = new Node[N+1];
-            lists = new List[N+1];
-            int total = 0;
-            for (int i = 1; i <= N; i++) {
-                lists[i] = new ArrayList<>();
-            }
 
+        while (T-- > 0) {
             st = new StringTokenizer(br.readLine());
-            for (int i = 1; i <= N; i++) {
-                int time = Integer.parseInt(st.nextToken());
-                nodes[i] = new Node(0, time, time);
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            int sum = 0;
+
+            int[] time = new int[n];
+            int[] max = new int[n];
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                time[i] = Integer.parseInt(st.nextToken());
+                max[i] = time[i];
             }
 
-            for (int i = 0; i < K; i++) {
+            int[][] arr = new int[n][n];
+            int[] in = new int[n];
+            for (int i = 0; i < k; i++) {
                 st = new StringTokenizer(br.readLine());
-                int from = Integer.parseInt(st.nextToken());
-                int to = Integer.parseInt(st.nextToken());
+                int s = Integer.parseInt(st.nextToken())-1;
+                int e = Integer.parseInt(st.nextToken())-1;
 
-                lists[from].add(to);
-                nodes[to].setEdge(nodes[to].getEdge()+1);
+                arr[s][e] = 1;
+                in[e]++;
             }
-            int must = Integer.parseInt(br.readLine());
+            int w = Integer.parseInt(br.readLine())-1;
 
-            Queue<Integer> q = new LinkedList<>();
-            for (int i = 1; i <= N; i++) {
-                //System.out.println(nodes[i]);
-                if(nodes[i].getEdge()==0){
-                    //System.out.println(i);
-                    q.offer(i);
-                }
+            Queue<Integer> q = new ArrayDeque<>();
+            for (int i = 0; i < n; i++) {
+                if (in[i] == 0) q.offer(i);
             }
 
-            loop:while (!q.isEmpty()){
-
-                int max = 0;
-
+            while (!q.isEmpty()) {
                 int cur = q.poll();
-                for (int i = 0; i < lists[cur].size(); i++) {
-                    nodes[lists[cur].get(i)].setMax(Math.max(nodes[lists[cur].get(i)].getMax(), nodes[cur].getMax()+nodes[lists[cur].get(i)].getTime()));
-                    nodes[lists[cur].get(i)].setEdge(nodes[lists[cur].get(i)].getEdge()-1);
-                    // 갱신된 노드의 진입차수가 0이면 큐에 노드 넣기
-                    if (nodes[lists[cur].get(i)].getEdge() == 0) {
-                        q.offer(lists[cur].get(i));
+                if (cur == w) break;
+                //sum += time[cur];
+
+                for (int i = 0; i < n; i++) {
+                    if (arr[cur][i] == 1) {
+                        max[i] = Math.max(max[i], max[cur]+time[i]);
+                        in[i]--;
+                        if (in[i] == 0) q.offer(i);
                     }
                 }
             }
 
-            sb.append(nodes[must].getMax()+"\n");
+            sb.append(max[w]).append("\n");
         }
-        System.out.println(sb);
+
+        System.out.print(sb);
     }
 }
