e3c243c7428d3c6d46f9e1ca5c249fcbb5cf95a6
NoDelay <57094856+youjiyeon@users.noreply.github.com>
2025-03-23 17:50:10 +0900

    [Gold IV] Title: 이모티콘, Time: 124 ms, Memory: 17080 KB -BaekjoonHub

--- 백준/Gold/14226. 이모티콘/이모티콘.java
+++ 백준/Gold/14226. 이모티콘/이모티콘.java
@@ -1,59 +1,53 @@
+import java.io.*;
 import java.util.*;
- 
+
 public class Main {
-    
-    static boolean[][] visited = new boolean[1001][1001];//[clipboard][total]
-    
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        int s = scan.nextInt();
-        
-        bfs(s);
-    }
-    
-    public static void bfs(int s) {
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(0, 1, 0));
-        visited[0][1] = true; 
-        
-        while(!q.isEmpty()) {
-            Node current = q.poll();
-            
-            if(current.total == s) {
-                System.out.println(current.time);
-                return;
-            }
-            
-            // 1. 화면에 있는 이모티콘 클립보드에 저장
-            q.offer(new Node(current.total, current.total, current.time + 1)); 
-            
-            
-            // 2. 클립보드에 있는 이모티콘 붙여넣기. 
-            // 클립보드 비어있지 않아야하고, 붙여넣은 후 개수가 총 개수보다 적어야 하며, 이전에 방문한적 없어야함.
-            if(current.clipboard != 0 && current.total + current.clipboard <= s && !visited[current.clipboard][current.total + current.clipboard]) {
-                q.offer(new Node(current.clipboard, current.total + current.clipboard, current.time + 1));
-                visited[current.clipboard][current.total + current.clipboard] = true;
-            }
-            
-            // 3. 화면에 있는 이모티콘 중 하나 삭제.
-            // 총 개수 1보다 크거나 같아야하고, 방문한적 없어야함.
-            if(current.total >= 1 && !visited[current.clipboard][current.total - 1]) {
-                q.offer(new Node(current.clipboard, current.total - 1, current.time + 1));
-                visited[current.clipboard][current.total - 1] = true;
-            }
+    static int S;
+    static class Node{
+        int desk;
+        int clip;
+        int time;
+        public Node(int desk, int clip, int time) {
+            this.desk = desk;
+            this.clip = clip;
+            this.time = time;
         }
     }
-    
-    public static class Node {
-        int clipboard;
-        int total;
-        int time;
-        
-        public Node(int clipboard, int total, int time) {
-            this.clipboard = clipboard;
-            this.total = total;
-            this.time = time;
+    static boolean[][] v = new boolean[1001][1001];
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        S = Integer.parseInt(br.readLine());
+        bfs();
+    }
+
+    static void bfs() {
+        Queue<Node> q = new ArrayDeque<>();
+        q.offer(new Node(1, 0, 0));
+        v[1][0] = true;
+
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+
+            if (cur.desk == S) {
+                System.out.println(cur.time);
+                return;
+            }
+
+            // 3개의 연산
+            // 1. 복사
+            q.offer(new Node(cur.desk, cur.desk, cur.time+1));
+
+            // 2. 붙여넣기
+            if (cur.clip != 0 && cur.desk+cur.clip <= S && !v[cur.desk+cur.clip][cur.clip]) {
+                q.offer(new Node(cur.desk+cur.clip, cur.clip, cur.time+1));
+                v[cur.desk+cur.clip][cur.clip] = true;
+            }
+
+            // 3. 삭제
+            if (cur.desk >= 1 && !v[cur.desk-1][cur.clip]) {
+                q.offer(new Node(cur.desk-1, cur.clip, cur.time+1));
+                v[cur.desk-1][cur.clip] = true;
+            }
         }
     }
 }
